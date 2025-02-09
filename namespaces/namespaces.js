53dd76c1ea6ab367949e4981c206ee3bd3769320
Saptarshi Datta <d.saptarshi2001@gmail.com>
2025-02-09 12:06:14 +0530

    Updated source committed

---
+++ namespaces/namespaces.js
@@ -0,0 +1,22 @@
+import express  from 'express';
+const app = express();
+// require('socket.io') = Server in the docs
+import socketio from 'socket.io';
+
+app.use(express.static(__dirname + '/public'));
+
+const expressServer = app.listen(8001);
+// io = the server object in the docs!
+const io = socketio(expressServer)
+
+// io = server in the docs
+io.of("/").on('connection',(socket)=>{
+    console.log(socket.id,"has connected")
+    //in ws we use "send" method, and it socket.io we use the "emit" method
+    // socket.emit('messageFromServer',{data:"Welcome to the socket server!"})
+    socket.on('newMessageToServer',(dataFromClient)=>{
+        console.log("Data:",dataFromClient);
+        io.emit('newMessageToClients',{text:dataFromClient.text});
+        // io.emit('newMessageToClients',{text:dataFromClient.text});
+    })
+})
